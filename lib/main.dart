4436925615f20323b2e914e640cbe0fbe09bcc48
namtv5 <namtv5@f88.vn>
2022-12-26 17:14:53 +0700

    sample call API

--- lib/main.dart
+++ lib/main.dart
@@ -1,70 +1,69 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
-  runApp(const FadeAppTest());
+  runApp(const SampleApp());
 }
 
-class FadeAppTest extends StatelessWidget {
-  const FadeAppTest({super.key});
-  // This widget is the root of your application.
+class SampleApp extends StatelessWidget {
+  const SampleApp({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Fade Demo',
+      title: 'Sample App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyFadeTest(title: 'Fade Demo'),
+      home: const SampleAppPage(),
     );
   }
 }
 
-class MyFadeTest extends StatefulWidget {
-  const MyFadeTest({super.key, required this.title});
+class SampleAppPage extends StatefulWidget {
+  const SampleAppPage({super.key});
 
-  final String title;
   @override
-  State<MyFadeTest> createState() => _MyFadeTest();
+  State<SampleAppPage> createState() => _SampleAppPageState();
 }
 
-class _MyFadeTest extends State<MyFadeTest> with TickerProviderStateMixin {
-  late AnimationController controller;
-  late CurvedAnimation curve;
+class _SampleAppPageState extends State<SampleAppPage> {
+  List widgets = [];
 
   @override
   void initState() {
     super.initState();
-    controller = AnimationController(
-      duration: const Duration(milliseconds: 2000),
-      vsync: this,
-    );
-    curve = CurvedAnimation(
-      parent: controller,
-      curve: Curves.easeIn,
-    );
+    loadData();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: const Text('Sample App'),
       ),
-      body: Center(
-        child: FadeTransition(
-          opacity: curve,
-          child: const FlutterLogo(
-            size: 100.0,
-          ),
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        tooltip: 'Fade',
-        onPressed: () {
-          controller.forward();
+      body: ListView.builder(
+        itemCount: widgets.length,
+        itemBuilder: (context, position) {
+          return getRow(position);
         },
-        child: const Icon(Icons.brush),
       ),
     );
+  }
+
+  Widget getRow(int i) {
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Text("Row ${widgets[i]["title"]}"),
+    );
+  }
+
+  Future<void> loadData() async {
+    var dataURL = Uri.parse('https://jsonplaceholder.typicode.com/posts');
+    http.Response response = await http.get(dataURL);
+    setState(() {
+      widgets = jsonDecode(response.body);
+    });
   }
 }
