297e310066ab85aa5a18e012c19797fa80c52984
namtv5 <namtv5@f88.vn>
2023-01-03 14:08:15 +0700

    sample call API with Isolate

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
 import 'dart:convert';
+import 'dart:isolate';
+import 'package:fluttertoast/fluttertoast.dart';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
@@ -9,6 +12,7 @@ void main() {
 
 class SampleApp extends StatelessWidget {
   const SampleApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -37,33 +41,96 @@ class _SampleAppPageState extends State<SampleAppPage> {
     loadData();
   }
 
+  Widget getBody() {
+    bool showLoadingDialog = widgets.isEmpty;
+    if (showLoadingDialog) {
+      return getProgressDialog();
+    } else {
+      return getListView();
+    }
+  }
+
+  Widget getProgressDialog() {
+    return const Center(child: CircularProgressIndicator());
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Sample App'),
       ),
-      body: ListView.builder(
-        itemCount: widgets.length,
-        itemBuilder: (context, position) {
-          return getRow(position);
-        },
-      ),
+      body: getBody(),
+    );
+  }
+
+  ListView getListView() {
+    return ListView.builder(
+      itemCount: widgets.length,
+      itemBuilder: (context, position) {
+        return getRow(position);
+      },
     );
   }
 
   Widget getRow(int i) {
-    return Padding(
-      padding: const EdgeInsets.all(10.0),
-      child: Text("Row ${widgets[i]["title"]}"),
-    );
+    return GestureDetector(
+        onTap: () {
+          Fluttertoast.showToast(
+              msg: "Onclick Item",
+              toastLength: Toast.LENGTH_SHORT,
+              gravity: ToastGravity.BOTTOM,
+              timeInSecForIosWeb: 1,
+              backgroundColor: Colors.red,
+              textColor: Colors.white,
+              fontSize: 16.0
+          );
+        },
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Text("Row ${widgets[i]["title"]}"),
+        ));
   }
 
   Future<void> loadData() async {
-    var dataURL = Uri.parse('https://jsonplaceholder.typicode.com/posts');
-    http.Response response = await http.get(dataURL);
+    ReceivePort receivePort = ReceivePort();
+    await Isolate.spawn(dataLoader, receivePort.sendPort);
+
+    // The 'echo' isolate sends its SendPort as the first message.
+    SendPort sendPort = await receivePort.first;
+
+    List msg = await sendReceive(
+      sendPort,
+      'https://jsonplaceholder.typicode.com/posts',
+    );
+
     setState(() {
-      widgets = jsonDecode(response.body);
+      widgets = msg;
     });
+  }
+
+  // The entry point for the isolate.
+  static Future<void> dataLoader(SendPort sendPort) async {
+    // Open the ReceivePort for incoming messages.
+    ReceivePort port = ReceivePort();
+
+    // Notify any other isolates what port this isolate listens to.
+    sendPort.send(port.sendPort);
+
+    await for (var msg in port) {
+      String data = msg[0];
+      SendPort replyTo = msg[1];
+
+      String dataURL = data;
+      http.Response response = await http.get(Uri.parse(dataURL));
+      // Lots of JSON to parse
+      replyTo.send(jsonDecode(response.body));
+    }
+  }
+
+  Future sendReceive(SendPort port, msg) {
+    ReceivePort response = ReceivePort();
+    port.send([msg, response.sendPort]);
+    return response.first;
   }
 }
